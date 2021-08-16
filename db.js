e50e01f5a76ad450aa67ad94b6831fb6597d1634
Nayanajith Bandaranayake <hbmnbandaranayake9988033@gmail.com>
2021-08-16 19:05:50 +0530

    Coverted a connection pool

--- db.js
+++ db.js
@@ -1,16 +1,12 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const dbConnection = mysql.createConnection({
+const dbConnection = mysql.createPool({
+  connectionLimit: 5,
   host: process.env.NODE_MYSQL_HOST,
   user: process.env.NODE_MYSQL_USER,
   password: process.env.NODE_MYSQL_PASSWORD,
   database: process.env.NODE_MYSQL_DATABASE,
-});
-
-dbConnection.connect((err) => {
-  if (err) console.log(err);
-  console.log("database connected.");
 });
 
 module.exports = dbConnection;
